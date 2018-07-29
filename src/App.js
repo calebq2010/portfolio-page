071b3f56547137ae7cf4041337408eee7ecd717e
Caleb <calebq2010@gmail.com>
2018-07-29 13:15:49 -0400

    added NavBar import and added basic navbar to site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { MenuItem } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,12 +13,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Button bsStyle="primary">primary</Button>
+
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">Caleb Quinn</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              about me
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              portfolio
+            </NavItem>
+            <NavItem eventKey={3} href="#">
+              contact me
+            </NavItem>
+          </Nav>
+        </Navbar>
+
           <h1 className="App-title">Caleb Quinns Portfolio Page In The Works!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
